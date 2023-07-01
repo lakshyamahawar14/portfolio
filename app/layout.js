6ca5432c372345e140444e3856e3ab51b0f1a23e
lakshyamahawar14 <lakshyamahawar14@gmail.com>
2023-07-02 00:33:54 +0530

    making changes for deployment

--- app/layout.js
+++ app/layout.js
@@ -2,10 +2,10 @@
 import { RecoilRoot } from "recoil";
 import "./globals.css";
 
-export const metadata = {
-  title: "Lakshya Mahawar's Portfolio",
-  description: "Portfolio for Lakshya Mahawar",
-};
+// export const metadata = {
+//   title: "Lakshya Mahawar's Portfolio",
+//   description: "Portfolio for Lakshya Mahawar",
+// };
 
 export default function RootLayout({ children }) {
   return (
