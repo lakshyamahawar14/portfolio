794b17da4df382977b05f07571a5518c7752b44a
lakshyamahawar14 <lakshyamahawar14@gmail.com>
2023-07-02 13:35:20 +0530

    updating contacts page

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -3,162 +3,200 @@ import React from "react";
 const Contact = () => {
   return (
     <>
-      <div className="contactContainer relative flex h-[auto] w-[auto] m-auto justify-center items-center">
-        <section className="flex justify-center items-cetner relative w-[100vw] h-[100vh] max-w-[1000px] usm:flex-col usm:hidden">
-          <div className="contactCard flex justify-center items-center m-auto w-[40%] h-[80%] flex-col bg-[#333333] rounded-xl">
-            <div className="w-[100%] h-[100%] px-[2vw] flex flex-col m-auto">
-              <div className="w-[auto] h-[auto]">
+      <div className="relative flex h-[auto] w-[auto] m-auto justify-center items-center">
+        <section className="contact relative overflow-y-hidden flex justify-center items-center h-[100vh] w-[100vw] px-[6vw] md:px-[3vw] usm:hidden">
+          <div className="contactCard relative flex justify-center items-center w-[100%] h-[100%] md:h-[80%] mx-[1rem] md:mx-[10px] max-w-[450px] max-h-[70vh] flex-col rounded-xl">
+            <div className="w-[100%] h-[100%] md:w-[90%] md:h-[80%] px-[2vw] flex flex-col m-auto">
+              <div className="flex flex-col justify-center items-start m-auto w-[100%]">
                 <div className="py-[20px]">
-                  <p className="text-[#9FEF00] text-[2rem] md:text-[1.7rem] sm:text-[1.3rem] text-center umd:text-[1.2rem]">
+                  <p className="text-[#9FEF00] text-[1.8rem] md:text-[1.7rem] sm:text-[1.3rem] text-center umd:text-[1.2rem]">
                     Add Description
                   </p>
                 </div>
               </div>
-              <div className="flex flex-col justify-center items-start m-auto w-[100%]">
-                <legend className="text-[0.9rem] py-[0.4rem]">Subject</legend>
-                <input
-                  type="text"
-                  name="subject"
-                  className="text-[1rem] caret-[#9FEF00] outline-none relative px-[10px] h-[35px] w-[100%] border-b-[2px] border-[#FFFFFF]"
-                ></input>
+              <div className="flex flex-col justify-center items-start py-[1rem] m-auto w-[100%]">
+                <fieldset className="border-2 pb-[10px] w-[100%] border-[#A4B1CD]">
+                  <legend className="text-[0.9rem] ml-[1rem] text-[#FCFCFC]">
+                    Subject
+                  </legend>
+                  <input
+                    type="text"
+                    name="subject"
+                    className="text-[1rem] caret-[#9FEF00] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#9FEF00]"
+                  ></input>
+                </fieldset>
               </div>
 
               <div className="flex flex-col justify-center items-start m-auto w-[100%]">
-                <legend className="text-[0.9rem] py-[0.4rem]">
-                  Description
-                </legend>
-                <textarea
-                  name="description"
-                  className="text-[1rem] caret-[#9FEF00] outline-none rounded-lg relative px-[10px] h-[35px] w-[100%] border-[2px] border-[#FFFFFF] min-h-[30vh]"
-                ></textarea>
+                <fieldset className="border-2 pb-[10px] w-[100%] border-[#A4B1CD]">
+                  <legend className="text-[0.9rem] ml-[1rem] text-[#FCFCFC]">
+                    Description
+                  </legend>
+                  <textarea
+                    type="text"
+                    name="description"
+                    className="text-[1rem] caret-[#9FEF00] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#9FEF00] min-h-[30vh]"
+                  ></textarea>
+                </fieldset>
               </div>
             </div>
           </div>
 
-          <div className="contactCard relative flex justify-center items-center m-auto w-[50%] h-[80%] flex-col bg-[#333333] rounded-xl">
-            <div className="w-[100%] h-[100%] px-[2vw] flex flex-col m-auto">
-              <div className="w-[auto] h-[auto]">
+          <div className="contactCard relative flex justify-center items-center w-[100%] h-[100%] md:h-[80%] mx-[1rem] md:mx-[10px] max-w-[450px] max-h-[70vh] flex-col bg-[transparent] rounded-xl">
+            <div className="w-[100%] h-[100%] md:w-[90%] md:h-[80%] px-[2vw] flex flex-col m-auto">
+              <div className="flex flex-col justify-center items-start m-auto w-[100%]">
                 <div className="py-[20px]">
-                  <p className="text-[#9FEF00] text-[2rem] md:text-[1.7rem] sm:text-[1.3rem] text-center umd:text-[1.2rem]">
-                    Contact Me
+                  <p className="text-[#9FEF00] text-[1.8rem] md:text-[1.7rem] sm:text-[1.3rem] text-center umd:text-[1.2rem]">
+                    Add Personal Details
                   </p>
                 </div>
               </div>
               <div className="flex flex-col justify-center items-start m-auto w-[100%]">
-                <legend className="text-[0.9rem] py-[0.4rem]">Name</legend>
-                <input
-                  type="text"
-                  name="name"
-                  className="text-[1rem] caret-[#9FEF00] outline-none relative px-[10px] h-[35px] w-[100%] border-b-[2px] border-[#FFFFFF]"
-                ></input>
+                <fieldset className="border-2 pb-[10px] w-[100%] border-[#A4B1CD]">
+                  <legend className="text-[0.9rem] ml-[1rem] text-[#FCFCFC]">
+                    Name
+                  </legend>
+                  <input
+                    type="text"
+                    name="name"
+                    className="text-[1rem] caret-[#9FEF00] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#9FEF00]"
+                  ></input>
+                </fieldset>
               </div>
               <div className="flex flex-col justify-center items-start m-auto w-[100%]">
-                <legend className="text-[0.9rem] py-[0.4rem]">Email</legend>
-                <input
-                  type="email"
-                  name="email"
-                  className="text-[1rem] caret-[#9FEF00] outline-none relative px-[10px] h-[35px] w-[100%] border-b-[2px] border-[#FFFFFF]"
-                ></input>
+                <fieldset className="border-2 pb-[10px] w-[100%] border-[#A4B1CD]">
+                  <legend className="text-[0.9rem] ml-[1rem] text-[#FCFCFC]">
+                    Email
+                  </legend>
+                  <input
+                    type="email"
+                    name="email"
+                    className="text-[1rem] caret-[#9FEF00] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#9FEF00]"
+                  ></input>
+                </fieldset>
               </div>
               <div className="flex flex-col justify-center items-start m-auto w-[100%]">
-                <legend className="text-[0.9rem] py-[0.4rem]">
-                  Mobile Number
-                </legend>
-                <input
-                  type="text"
-                  name="number"
-                  className="text-[1rem] caret-[#9FEF00] outline-none relative px-[10px] h-[35px] w-[100%] border-b-[2px] border-[#FFFFFF]"
-                ></input>
+                <fieldset className="border-2 pb-[10px] w-[100%] border-[#A4B1CD]">
+                  <legend className="text-[0.9rem] ml-[1rem] text-[#FCFCFC]">
+                    Phone Number
+                  </legend>
+                  <input
+                    type="text"
+                    name="number"
+                    className="text-[1rem] caret-[#9FEF00] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#9FEF00]"
+                  ></input>
+                </fieldset>
               </div>
               <div className="flex flex-col justify-center items-start m-auto w-[100%]">
-                <legend className="text-[0.9rem] py-[0.4rem]">
-                  Profession
-                </legend>
-                <input
-                  type="text"
-                  name="profession"
-                  className="text-[1rem] caret-[#9FEF00] outline-none relative px-[10px] h-[35px] w-[100%] border-b-[2px] border-[#FFFFFF]"
-                ></input>
+                <fieldset className="border-2 pb-[10px] w-[100%] border-[#A4B1CD]">
+                  <legend className="text-[0.9rem] ml-[1rem] text-[#FCFCFC]">
+                    Profession
+                  </legend>
+                  <input
+                    type="text"
+                    name="profession"
+                    className="text-[1rem] caret-[#9FEF00] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#9FEF00]"
+                  ></input>
+                </fieldset>
               </div>
-              <div className="flex flex-col justify-center items-center m-auto">
+              <div className="flex flex-col justify-center items-center m-auto py-[1rem]">
                 <input
                   type="submit"
                   value={"Contact"}
-                  className="cursor-pointer outline-none border-none rounded-[50px] hover:bg-[#FFFFFF] button-transition h-[35px] w-[auto] px-[1rem] bg-[#9FEF00] text-[#101010]"
+                  className="cursor-pointer outline-none border-none rounded-[50px] hover:bg-[#FCFCFC] button-transition h-[35px] w-[auto] px-[1rem] bg-[#9FEF00] text-[#101010]"
                 ></input>
               </div>
             </div>
           </div>
         </section>
 
-        <section className="contact justify-center items-cetner relative w-[100vw] h-[auto] usm:flex usm:flex-col hidden">
-          <div className="contactCard relative flex justify-center items-center m-auto w-[50%] h-[80%] flex-col bg-[#333333] rounded-xl usm:w-[90%] usm:h-[100vh] usm:my-[1rem]">
-            <div className="w-[auto] h-[auto]">
-              <div className="py-[20px]">
-                <p className="text-[#9FEF00] text-[2rem] md:text-[1.7rem] sm:text-[1.3rem] text-center umd:text-[1.2rem]">
-                  Contact Me
-                </p>
-              </div>
-            </div>
+        <section className="contact justify-center overflow-y-hidden items-center relative w-[100vw] h-[auto] min-h-[100vh] usm:flex usm:flex-col hidden">
+          <div className="contactCard relative flex justify-center items-center m-auto w-[90%] h-[90%] flex-col bg-[transparent] rounded-xl">
             <div className="w-[100%] h-[100%] px-[6vw] flex flex-col m-auto mb-[1rem]">
               <div className="flex flex-col justify-center items-start m-auto w-[100%]">
-                <legend className="text-[0.9rem] py-[0.4rem]">Name</legend>
-                <input
-                  type="text"
-                  name="name"
-                  className="text-[1rem] caret-[#9FEF00] outline-none relative px-[10px] h-[35px] w-[100%] border-b-[2px] border-[#FFFFFF]"
-                ></input>
+                <div className="py-[20px]">
+                  <p className="text-[#9FEF00] text-[1.8rem] md:text-[1.7rem] sm:text-[1.3rem] text-center umd:text-[1.2rem]">
+                    Contact Me
+                  </p>
+                </div>
               </div>
               <div className="flex flex-col justify-center items-start m-auto w-[100%]">
-                <legend className="text-[0.9rem] py-[0.4rem]">Email</legend>
-                <input
-                  type="email"
-                  name="email"
-                  className="text-[1rem] caret-[#9FEF00] outline-none relative px-[10px] h-[35px] w-[100%] border-b-[2px] border-[#FFFFFF]"
-                ></input>
+                <fieldset className="border-2 pb-[10px] w-[100%] border-[#A4B1CD]">
+                  <legend className="text-[0.9rem] ml-[1rem] text-[#FCFCFC]">
+                    Name
+                  </legend>
+                  <input
+                    type="text"
+                    name="name"
+                    className="text-[1rem] caret-[#9FEF00] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#9FEF00]"
+                  ></input>
+                </fieldset>
               </div>
               <div className="flex flex-col justify-center items-start m-auto w-[100%]">
-                <legend className="text-[0.9rem] py-[0.4rem]">
-                  Mobile Number
-                </legend>
-                <input
-                  type="text"
-                  name="number"
-                  className="text-[1rem] caret-[#9FEF00] outline-none relative px-[10px] h-[35px] w-[100%] border-b-[2px] border-[#FFFFFF]"
-                ></input>
+                <fieldset className="border-2 pb-[10px] w-[100%] border-[#A4B1CD]">
+                  <legend className="text-[0.9rem] ml-[1rem] text-[#FCFCFC]">
+                    Email
+                  </legend>
+                  <input
+                    type="email"
+                    name="email"
+                    className="text-[1rem] caret-[#9FEF00] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#9FEF00]"
+                  ></input>
+                </fieldset>
               </div>
               <div className="flex flex-col justify-center items-start m-auto w-[100%]">
-                <legend className="text-[0.9rem] py-[0.4rem]">
-                  Profession
-                </legend>
-                <input
-                  type="text"
-                  name="profession"
-                  className="text-[1rem] caret-[#9FEF00] outline-none relative px-[10px] h-[35px] w-[100%] border-b-[2px] border-[#FFFFFF]"
-                ></input>
+                <fieldset className="border-2 pb-[10px] w-[100%] border-[#A4B1CD]">
+                  <legend className="text-[0.9rem] ml-[1rem] text-[#FCFCFC]">
+                    Phone Number
+                  </legend>
+                  <input
+                    type="text"
+                    name="number"
+                    className="text-[1rem] caret-[#9FEF00] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#9FEF00]"
+                  ></input>
+                </fieldset>
               </div>
               <div className="flex flex-col justify-center items-start m-auto w-[100%]">
-                <legend className="text-[0.9rem] py-[0.4rem]">Subject</legend>
-                <input
-                  type="text"
-                  name="subject"
-                  className="text-[1rem] caret-[#9FEF00] outline-none relative px-[10px] h-[35px] w-[100%] border-b-[2px] border-[#FFFFFF]"
-                ></input>
+                <fieldset className="border-2 pb-[10px] w-[100%] border-[#A4B1CD]">
+                  <legend className="text-[0.9rem] ml-[1rem] text-[#FCFCFC]">
+                    Profession
+                  </legend>
+                  <input
+                    type="text"
+                    name="profession"
+                    className="text-[1rem] caret-[#9FEF00] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#9FEF00]"
+                  ></input>
+                </fieldset>
               </div>
               <div className="flex flex-col justify-center items-start m-auto w-[100%]">
-                <legend className="text-[0.9rem] py-[0.4rem]">
-                  Description
-                </legend>
-                <textarea
-                  name="description"
-                  className="text-[1rem] rounded-lg caret-[#9FEF00] outline-none relative px-[10px] w-[100%] border-[2px] border-[#FFFFFF] h-[auto] min-h-[20vh]"
-                ></textarea>
+                <fieldset className="border-2 pb-[10px] w-[100%] border-[#A4B1CD]">
+                  <legend className="text-[0.9rem] ml-[1rem] text-[#FCFCFC]">
+                    Subject
+                  </legend>
+                  <input
+                    type="text"
+                    name="subject"
+                    className="text-[1rem] caret-[#9FEF00] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#9FEF00]"
+                  ></input>
+                </fieldset>
+              </div>
+              <div className="flex flex-col justify-center items-start m-auto w-[100%]">
+                <fieldset className="border-2 pb-[10px] w-[100%] border-[#A4B1CD]">
+                  <legend className="text-[0.9rem] ml-[1rem] text-[#FCFCFC]">
+                    Description
+                  </legend>
+                  <textarea
+                    type="text"
+                    name="description"
+                    className="text-[1rem] caret-[#9FEF00] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#9FEF00] min-h-[20vh]"
+                  ></textarea>
+                </fieldset>
               </div>
               <div className="flex flex-col justify-center items-center m-auto">
                 <input
                   type="submit"
                   value={"Contact"}
-                  className="cursor-pointer outline-none border-none rounded-[50px] hover:bg-[#FFFFFF] button-transition h-[35px] w-[auto] px-[1rem] bg-[#9FEF00] text-[#101010]"
+                  className="cursor-pointer outline-none border-none rounded-[50px] hover:bg-[#FFFFFF] button-transition h-[35px] w-[auto] mt-[1rem] px-[1rem] bg-[#9FEF00] text-[#101010]"
                 ></input>
               </div>
             </div>
