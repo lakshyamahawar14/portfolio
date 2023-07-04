8218587ea15754f9f4c45d147a2a18552063b8ad
lakshyamahawar14 <lakshyamahawar14@gmail.com>
2023-07-04 11:51:54 +0530

    updating contact code

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -3,206 +3,132 @@ import React from "react";
 const Contact = () => {
   return (
     <>
-      <section className="contact relative flex flex-col justify-center items-center py-[8vh] h-[100vh] w-[100vw] px-[6vw] usm:hidden bg-[#56cae1]">
-        <div className="w-[100%] max-w-[1000px] flex justify-center items-center h-[100%]">
-          <div className="contactCard relative flex justify-start items-center w-[100%] h-[100%] mx-[1rem] flex-col bg-[transparent] rounded-xl">
-            <div className="h-[20%] flex flex-col justify-center items-center w-[100%]">
-              <div className="py-[10px]">
-                <p className="text-[#0c3134] text-[1.8rem] rubik font-[800] tracking-wide text-center">
-                  Details
-                </p>
-              </div>
-            </div>
-            <div className="w-[100%] h-[100%] px-[2vw] flex flex-col ">
-              <div className="flex flex-col justify-start items-start py-[1rem] h-[100%] w-[100%]">
-                <fieldset className="border-[2px] pb-[10px] border-[#0c3134] w-[100%]">
-                  <legend className="text-[0.9rem] ml-[1rem] text-[#0c3134] ubuntu font-[700]">
-                    Name
-                  </legend>
-                  <input
-                    type="text"
-                    name="name"
-                    className="text-[1rem] caret-[#0c3134] outline-none relative ml-[10px] px-[10px] h-[35px] w-[100%] broder-none border-b-[2px] border-[#0c3134] text-[#0c3134]"
-                  ></input>
-                </fieldset>
-              </div>
-              <div className="flex flex-col justify-start items-start py-[1rem] h-[100%] w-[100%]">
-                <fieldset className="border-[2px] pb-[10px] border-[#0c3134] w-[100%]">
-                  <legend className="text-[0.9rem] ml-[1rem] text-[#0c3134] ubuntu font-[700]">
-                    Email
-                  </legend>
-                  <input
-                    type="text"
-                    name="email"
-                    className="text-[1rem] caret-[#0c3134] outline-none relative ml-[10px] px-[10px] h-[35px] w-[100%] broder-none border-b-[2px] border-[#0c3134] text-[#0c3134]"
-                  ></input>
-                </fieldset>
-              </div>
-              <div className="flex flex-col justify-start items-start py-[1rem] h-[100%] w-[100%]">
-                <fieldset className="border-[2px] pb-[10px] border-[#0c3134] w-[100%]">
-                  <legend className="text-[0.9rem] ml-[1rem] text-[#0c3134] ubuntu font-[700]">
-                    Phone Number
-                  </legend>
-                  <input
-                    type="text"
-                    name="phone"
-                    className="text-[1rem] caret-[#0c3134] outline-none relative ml-[10px] px-[10px] h-[35px] w-[100%] broder-none border-b-[2px] border-[#0c3134] text-[#0c3134]"
-                  ></input>
-                </fieldset>
-              </div>
-              <div className="flex flex-col justify-start items-start py-[1rem] h-[100%]  w-[100%]">
-                <fieldset className="border-[2px] pb-[10px] border-[#0c3134] w-[100%]">
-                  <legend className="text-[0.9rem] ml-[1rem] text-[#0c3134] ubuntu font-[700]">
-                    Profession
-                  </legend>
-                  <input
-                    type="text"
-                    name="profession"
-                    className="text-[1rem] caret-[#0c3134] outline-none relative ml-[10px] px-[10px] h-[35px] w-[100%] broder-none border-b-[2px] border-[#0c3134] text-[#0c3134]"
-                  ></input>
-                </fieldset>
-              </div>
-            </div>
-          </div>
-
-          <div className="contactCard min-h-[60vh] relative flex justify-start items-center w-[100%] h-[100%] mx-[1rem] flex-col bg-[transparent] rounded-xl">
-            <div className="flex h-[20%] flex-col justify-center items-center w-[100%]">
-              <div className="py-[10px]">
-                <p className="text-[#0c3134] text-[1.8rem] rubik font-[800] tracking-wide text-center">
-                  Description
-                </p>
-              </div>
-            </div>
-            <div className="w-[100%] h-[100%] px-[2vw] flex flex-col justify-center items-start">
-              <div className="flex flex-col justify-start items-start py-[1rem] h-[30%] w-[100%]">
-                <fieldset className="border-[2px] pb-[10px] border-[#0c3134] w-[100%]">
-                  <legend className="text-[0.9rem] ml-[1rem] text-[#0c3134] ubuntu font-[700]">
-                    Subject
-                  </legend>
-                  <input
-                    type="text"
-                    name="subject"
-                    className="text-[1rem] caret-[#0c3134] outline-none relative ml-[10px] px-[10px] h-[35px] w-[100%] broder-none border-b-[2px] border-[#0c3134] text-[#0c3134]"
-                  ></input>
-                </fieldset>
-              </div>
-              <div className="flex flex-col justify-start items-start py-[1rem] h-[100%] w-[100%]">
-                <fieldset className="border-[2px] pb-[10px] border-[#0c3134] w-[100%] h-[60%]">
-                  <legend className="text-[0.9rem] ml-[1rem] text-[#0c3134] ubuntu font-[700]">
-                    Description
-                  </legend>
-                  <textarea
-                    type="text"
-                    name="description"
-                    className="text-[1rem] caret-[#0c3134] outline-none relative ml-[10px] px-[10px] h-[100%] w-[100%] broder-none border-b-[2px] border-[#0c3134] text-[#0c3134]"
-                  ></textarea>
-                </fieldset>
-              </div>
-            </div>
-          </div>
+      <div className="isolate bg-[#56cae1] px-6 py-24 sm:py-32 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl text-[#0e2f36] sm:text-4xl rubik font-[700] tracking-wide">
+            Contact Me
+          </h2>
         </div>
-
-        <div className="flex flex-col justify-center items-center h-[20%] w-[100%]">
-          <input
-            type="submit"
-            value={"Contact"}
-            className="cursor-pointer outline-none border-none rounded-[50px] hover:bg-[#FCFCFC] hover:text-[#0c3134] button-transition h-[35px] w-[auto] px-[1rem] bg-[#0c3134] text-[#FCFCFC]"
-          ></input>
-        </div>
-      </section>
-
-      <section className="contact justify-center overflow-y-hidden items-center py-[8vh] relative w-[100vw] h-[100%] min-h-[100vh] usm:flex usm:flex-col hidden bg-[#56cae1]">
-        <div className="contactCard relative flex justify-center items-center  w-[100%] h-[100%] flex-col bg-[transparent] rounded-xl">
-          <div className="flex flex-col justify-center items-center  w-[auto]">
-            <div className="py-[20px]">
-              <p className="text-[#0c3134] text-[1.3rem] rubik font-[800] tracking-wide text-center">
-                Contact Me
-              </p>
-            </div>
-          </div>
-          <div className="w-[100%] h-[100%] px-[6vw] flex flex-col  mb-[1rem]">
-            <div className="flex flex-col justify-center items-start  w-[100%]">
-              <fieldset className="border-2 pb-[10px] w-[100%] border-[#0c3134]">
-                <legend className="text-[0.9rem] ubuntu font-[700] ml-[1rem] text-[#0c3134]">
-                  Name
-                </legend>
+        <form className="mx-auto mt-16 max-w-xl sm:mt-20">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div>
+              <label
+                for="first-name"
+                className="block text-sm leading-6 text-[#0e2f36] ubuntu tracking-wide font-[700]"
+              >
+                First name
+              </label>
+              <div className="mt-2.5">
                 <input
                   type="text"
-                  name="name"
-                  className="text-[1rem] caret-[#0c3134] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#0c3134] text-[#0c3134]"
-                ></input>
-              </fieldset>
+                  name="first-name"
+                  id="first-name"
+                  autocomplete="given-name"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-[#0e2f36] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  required
+                />
+              </div>
             </div>
-            <div className="flex flex-col justify-center items-start  w-[100%]">
-              <fieldset className="border-2 pb-[10px] w-[100%] border-[#0c3134]">
-                <legend className="text-[0.9rem] ubuntu font-[700] ml-[1rem] text-[#0c3134]">
-                  Email
-                </legend>
-                <input
-                  type="email"
-                  name="email"
-                  className="text-[1rem] caret-[#0c3134] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#0c3134] text-[#0c3134]"
-                ></input>
-              </fieldset>
-            </div>
-            <div className="flex flex-col justify-center items-start  w-[100%]">
-              <fieldset className="border-2 pb-[10px] w-[100%] border-[#0c3134]">
-                <legend className="text-[0.9rem] ubuntu font-[700] ml-[1rem] text-[#0c3134]">
-                  Phone Number
-                </legend>
+            <div>
+              <label
+                for="last-name"
+                className="block text-sm leading-6 ubuntu tracking-wide font-[700] text-[#0e2f36]"
+              >
+                Last name
+              </label>
+              <div className="mt-2.5">
                 <input
                   type="text"
-                  name="number"
-                  className="text-[1rem] caret-[#0c3134] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#0c3134] text-[#0c3134]"
-                ></input>
-              </fieldset>
+                  name="last-name"
+                  id="last-name"
+                  autocomplete="family-name"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  required
+                />
+              </div>
             </div>
-            <div className="flex flex-col justify-center items-start  w-[100%]">
-              <fieldset className="border-2 pb-[10px] w-[100%] border-[#0c3134]">
-                <legend className="text-[0.9rem] ubuntu font-[700] ml-[1rem] text-[#0c3134]">
-                  Profession
-                </legend>
+            <div className="sm:col-span-2">
+              <label
+                for="company"
+                className="block text-sm leading-6 ubuntu tracking-wide font-[700] text-[#0e2f36]"
+              >
+                Profession
+              </label>
+              <div className="mt-2.5">
                 <input
                   type="text"
                   name="profession"
-                  className="text-[1rem] caret-[#0c3134] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#0c3134] text-[#0c3134]"
-                ></input>
-              </fieldset>
+                  id="profession"
+                  autocomplete="profession"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  required
+                />
+              </div>
             </div>
-            <div className="flex flex-col justify-center items-start  w-[100%]">
-              <fieldset className="border-2 pb-[10px] w-[100%] border-[#0c3134]">
-                <legend className="text-[0.9rem] ubuntu font-[700] ml-[1rem] text-[#0c3134]">
-                  Subject
-                </legend>
+            <div className="sm:col-span-2">
+              <label
+                for="email"
+                className="block text-sm leading-6 ubuntu tracking-wide font-[700] text-[#0e2f36]"
+              >
+                Email
+              </label>
+              <div className="mt-2.5">
                 <input
-                  type="text"
-                  name="subject"
-                  className="text-[1rem] caret-[#0c3134] outline-none relative ml-[10px] px-[10px] h-[35px] w-[90%] broder-none border-b-[2px] border-[#0c3134] text-[#0c3134]"
-                ></input>
-              </fieldset>
+                  type="email"
+                  name="email"
+                  id="email"
+                  autocomplete="email"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  required
+                />
+              </div>
             </div>
-            <div className="flex flex-col justify-center items-start  w-[100%]">
-              <fieldset className="border-2 pb-[10px] w-[100%] border-[#0c3134]">
-                <legend className="text-[0.9rem] ubuntu font-[700] ml-[1rem] text-[#0c3134]">
-                  Description
-                </legend>
+            <div className="sm:col-span-2">
+              <label
+                for="phone"
+                className="block text-sm leading-6 ubuntu tracking-wide font-[700] text-[#0e2f36]"
+              >
+                Phone Number
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="phone"
+                  name="phone"
+                  id="phone"
+                  autocomplete="phone"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-white"
+                  required
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                for="message"
+                className="block text-sm leading-6 ubuntu tracking-wide font-[700] text-[#0e2f36]"
+              >
+                Message
+              </label>
+              <div className="mt-2.5">
                 <textarea
-                  type="text"
-                  name="description"
-                  className="text-[1rem] caret-[#0c3134] outline-none relative ml-[10px] px-[10px] h-[35px] w-[1000%] broder-none border-b-[2px] border-[#0c3134] min-h-[20vh] text-[#0c3134]"
+                  name="message"
+                  id="message"
+                  rows="4"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  required
                 ></textarea>
-              </fieldset>
-            </div>
-            <div className="flex flex-col justify-center items-center ">
-              <input
-                type="submit"
-                value={"Contact"}
-                className="cursor-pointer outline-none border-none rounded-[35px] hover:bg-[#FCFCFC] hover:text-[#0c3134] button-transition h-[35px] w-[auto] mt-[0.8rem] px-[0.8rem] bg-[#0c3134] text-[#FCFCFC]"
-              ></input>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+          <div className="mt-10">
+            <button
+              type="submit"
+              className="block w-[auto] bg-[#970bde] rounded-md ubuntu tracking-wide font-[700] px-3.5 py-2.5 text-center text-sm shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 text-[#fcfcfc]"
+            >
+              Let's talk
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
