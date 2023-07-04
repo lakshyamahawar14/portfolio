6bc5fd4aa4f353704d8ab932c4f2683832b70310
lakshyamahawar14 <lakshyamahawar14@gmail.com>
2023-07-04 12:30:31 +0530

    changing contact

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -124,7 +124,7 @@ const Contact = () => {
               type="submit"
               className="block w-[auto] bg-[#970bde] rounded-md ubuntu tracking-wide font-[700] px-3.5 py-2.5 text-center text-sm shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 text-[#fcfcfc]"
             >
-              Let's talk
+              Let&apos;s talk
             </button>
           </div>
         </form>
