1e8614e217ae5bb53c20576b7bb3cdeab0eb12cc
lakshyamahawar14 <lakshyamahawar14@gmail.com>
2023-07-11 01:01:21 +0530

    updating description of html pages

--- app/about/layout.js
+++ app/about/layout.js
@@ -3,7 +3,7 @@ import "../globals.css";
 export const metadata = {
   title: "About Me",
   description:
-    "About Page for Lakshya Mahawar's Portfolio. This portfolio website is made using NextJS framework.",
+    "About Page for Lakshya Mahawar's Portfolio. This portfolio website is made using NextJS 13 framework, which is a full stack web developemnt framework and is a successor to React framework. This is a personal website to display the resume, skills, projects of Lakshya Mahawar. This website have links like About Me, My Projects, My Skills, Contact Me. The website us fully responsive because Tailwind CSS is used.",
 };
 
 export default function RootLayout({ children }) {
--- app/skills/layout.js
+++ app/skills/layout.js
@@ -3,7 +3,7 @@ import "../globals.css";
 export const metadata = {
   title: "My Skills",
   description:
-    "Expected Skill Level of Lakshya Mahawar. This portfolio website is made using NextJS framework.",
+    "Expected Skill Level of Lakshya Mahawar. This portfolio website is made using NextJS 13 framework, which is a full stack web developemnt framework and is a successor to React framework. This is a personal website to display the resume, skills, projects of Lakshya Mahawar. This website have links like About Me, My Projects, My Skills, Contact Me. The website us fully responsive because Tailwind CSS is used.",
 };
 
 export default function RootLayout({ children }) {
